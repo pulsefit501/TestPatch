7dca78f875d5b27ff503248dd91e3a501b0d4894
pulsefit501 <110103775+pulsefit501@users.noreply.github.com>
2022-08-22 12:50:21 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+ # required_version = ">= 0.14.9"
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+ #text
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["vpc_sg_01"]
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
